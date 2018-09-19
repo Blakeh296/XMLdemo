46941cf054c9f91ef9a393c9e5c3656ccc39ad53
Blakeh296 <bhoelle216@gmail.com>
2018-09-19 18:32:49 -0400

    Working, very basic

---
+++ XMLdemo/XMLdemo/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="XMLdemo.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnLoad" runat="server" OnClick="btnLoad_Click" Text="Load" />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+            </asp:GridView>
+        </div>
+        <asp:XmlDataSource ID="XmlDataSource1" runat="server" DataFile="~/MOCK_DATA.xml"></asp:XmlDataSource>
+    </form>
+</body>
+</html>
---
+++ XMLdemo/XMLdemo/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace XMLdemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLoad_Click(object sender, EventArgs e)
+        {
+            DataSet dsXML = new DataSet();
+            dsXML.ReadXml(Server.MapPath("/") + "MOCK_DATA.xml");
+            GridView1.AutoGenerateColumns = true;
+            GridView1.DataSource = dsXML;
+            GridView1.DataBind();
+        }
+    }
+}
