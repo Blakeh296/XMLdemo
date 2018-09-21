cf872a1b515c5ee58eb4ad0d4167148c8029981f
Blake Hoelle <bhoelle216@gmail.com>
2018-09-20 21:31:28 -0400

    Commit

--- XMLdemo/XMLdemo/Default.aspx.cs
+++ XMLdemo/XMLdemo/Default.aspx.cs
@@ -28,49 +28,71 @@ namespace XMLdemo
 
         protected void btnGetSelected_Click(object sender, EventArgs e)
         {
-            tbID.Text = GridView1.SelectedRow.Cells[1].Text;
+            try
+            {
+                tbID.Text = GridView1.SelectedRow.Cells[0].Text;
 
-            tbFirstName.Text = GridView1.SelectedRow.Cells[2].Text;
+                tbFirstName.Text = GridView1.SelectedRow.Cells[1].Text;
 
-            tbLastName.Text = GridView1.SelectedRow.Cells[3].Text;
+                tbLastName.Text = GridView1.SelectedRow.Cells[3].Text;
 
-            tbEmail.Text = GridView1.SelectedRow.Cells[4].Text;
+                tbEmail.Text = GridView1.SelectedRow.Cells[4].Text;
 
-            tbGender.Text = GridView1.SelectedRow.Cells[5].Text;
+                tbGender.Text = GridView1.SelectedRow.Cells[5].Text;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         protected void btnLoad_Click(object sender, EventArgs e)
         {
-            //This will work when the DataGridView doesn't have a datasource
-            DataSet dsXML = new DataSet();
-            //Find the File to append to the dataset
-            dsXML.ReadXml(Server.MapPath("/") + "MOCK_DATA.xml");
-            //You don't kknow how many columns to use, set AutoGen to true
-            GridView1.AutoGenerateColumns = true;
-            //Set the GV Datasource equal to the DataSet
-            GridView1.DataSource = dsXML;
+            try
+            {
+                //This will work when the DataGridView doesn't have a datasource
+                DataSet dsXML = new DataSet();
+                //Find the File to append to the dataset
+                dsXML.ReadXml(Server.MapPath("/") + "MOCK_DATA.xml");
+                //You don't kknow how many columns to use, set AutoGen to true
+                GridView1.AutoGenerateColumns = true;
+                //Set the GV Datasource equal to the DataSet
+                GridView1.DataSource = dsXML;
 
-            //Bind the DataSet with the GridView
-            GridView1.DataBind();
+                //Bind the DataSet with the GridView
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
         }
 
         protected void btnUpdateGV_Click(object sender, EventArgs e)
         {
-            XmlDocument document = new XmlDocument();
-            document.Load(@"G:\C#\XMLdemo\XMLdemo\XMLdemo\MOCK_DATA.xml");
-            XmlElement contactElement = document.CreateElement("first_Name");
-            XmlText txt = document.CreateTextNode(tbFirstName.Text);
-            document.DocumentElement.AppendChild(contactElement);
-            document.DocumentElement.LastChild.AppendChild(txt);
+            try
+            {
+                XmlDocument document = new XmlDocument();
+                document.Load(@"G:\C#\XMLdemo\XMLdemo\XMLdemo\MOCK_DATA.xml");
+                XmlElement contactElement = document.CreateElement("MOCK_DATA");
+                XmlAttribute attribute = document.CreateAttribute("first_Name");
+                attribute.Value = tbFirstName.Text;
+                document.Save("MOCK_DATA.xml");
 
-           // document.SelectSingleNode(@"/DataSet/MOCK_DATA[2]").Attributes["first_name"].InnerText = tbFirstName.Text;
+                //document.SelectSingleNode(@"/DataSet/MOCK_DATA[2]").Attributes["first_name"].InnerText = tbFirstName.Text;
 
-            GridView1.SelectedRow.Cells[2].Text = tbFirstName.Text;
+                GridView1.SelectedRow.Cells[2].Text = tbFirstName.Text;
 
-            GridView1.SelectedRow.Cells[3].Text = tbLastName.Text;
+                GridView1.SelectedRow.Cells[3].Text = tbLastName.Text;
 
-            GridView1.SelectedRow.Cells[4].Text = tbEmail.Text;
-
+                GridView1.SelectedRow.Cells[4].Text = tbEmail.Text;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+
     }
 }
